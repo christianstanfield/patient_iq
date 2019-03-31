e0670e646cfffe28ff3aa1ba0dad84735e85351e
christianstanfield <christian.stanfield@gmail.com>
2019-03-31 18:55:01 -0500

    Company-level management of employees without authorization
    Also created Top Earning Employees table for top 3 users by salary by department

--- app/models/company.rb
+++ app/models/company.rb
@@ -4,4 +4,17 @@ class Company < ApplicationRecord
   has_many :users, through: :departments
 
   validates_presence_of :name
+
+  def top_employees
+    departments.joins(
+      <<~SQL
+        join lateral (
+        select * from users
+        where department_id = departments.id
+        order by salary desc
+        limit 3
+        ) u on true
+      SQL
+    ).order(:name).select('departments.*, u.*')
+  end
 end
