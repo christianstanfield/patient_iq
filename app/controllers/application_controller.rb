a3e9311391cb7ee73c17f4672bd8e373dea2bab3
christianstanfield <christian.stanfield@gmail.com>
2019-04-01 18:50:46 -0500

    Initial pundit setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  include Pundit
+
+  after_action :verify_authorized, unless: :sessions_controller?
 
   private
 
@@ -7,4 +10,8 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find_by id: session[:current_user_id]
   end
   helper_method :current_user
+
+  def sessions_controller?
+    controller_name == 'sessions'
+  end
 end
